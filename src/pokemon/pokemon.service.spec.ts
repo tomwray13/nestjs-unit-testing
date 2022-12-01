92edf48863f77b0eac48b5fc3d89ea8ac99cf692
Tom Ray <tomwray13@gmail.com>
2022-12-01 09:45:53 +0000

    Add http tests

--- src/pokemon/pokemon.service.spec.ts
+++ src/pokemon/pokemon.service.spec.ts
@@ -1,18 +1,25 @@
-import { HttpModule } from '@nestjs/axios';
-import { BadRequestException } from '@nestjs/common';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
+import { HttpService } from '@nestjs/axios';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonService } from './pokemon.service';
 
 describe('PokemonService', () => {
   let pokemonService: PokemonService;
+  let httpService: DeepMocked<HttpService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule],
       providers: [PokemonService],
-    }).compile();
+    })
+      .useMocker(createMock)
+      .compile();
 
     pokemonService = module.get<PokemonService>(PokemonService);
+    httpService = module.get(HttpService);
   });
 
   it('should be defined', () => {
@@ -33,9 +40,35 @@ describe('PokemonService', () => {
     });
 
     it('valid pokemon ID to return the pokemon name', async () => {
+      httpService.axiosRef.mockResolvedValueOnce({
+        data: {
+          species: { name: `bulbasaur` },
+        },
+        headers: {},
+        config: { url: '' },
+        status: 200,
+        statusText: '',
+      });
+
       const getPokemon = pokemonService.getPokemon(1);
 
       await expect(getPokemon).resolves.toBe('bulbasaur');
+    });
+
+    it('if Pokemon API response unexpectedly changes, throw an error', async () => {
+      httpService.axiosRef.mockResolvedValueOnce({
+        data: `Unexpected data`,
+        headers: {},
+        config: { url: '' },
+        status: 200,
+        statusText: '',
+      });
+
+      const getPokemon = pokemonService.getPokemon(1);
+
+      await expect(getPokemon).rejects.toBeInstanceOf(
+        InternalServerErrorException,
+      );
     });
   });
 });
